d3cd0ead9f1288b7e62cd392c4ebefa8a11ad255
Simran <srivastavasimran1912@gmail.com>
2024-07-04 15:10:29 +0000

    submission test

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home - CI/CD Pipeline</h3>"
+    html = "<h3>Change test - Sklearn Prediction Home - CI/CD Pipeline</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
